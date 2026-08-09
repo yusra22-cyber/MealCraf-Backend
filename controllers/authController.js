60f1e4ad039f52c5383177d3b8588f7b7becd341
Yusra <laraibyusra519@gmail.com>
2026-08-09 08:01:04 -0700

    Cors issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,8 +29,8 @@ async function handleLogin(req,res){
 
         res.cookie("token",token,{
             httpOnly:true,
-            sameSite:"none",
-            secure:true,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: process.env.NODE_ENV === "production",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -82,8 +82,8 @@ async function handleLogout(req,res){
        res.clearCookie("token",
         {
             httpOnly:true,
-            sameSite:"none",
-            secure:true,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: process.env.NODE_ENV === "production",
         }
         )
 
