7efd35ea69a754be8ebb574f7e0ebf36dfc91f4c
Yusra <laraibyusra519@gmail.com>
2026-08-09 07:36:29 -0700

    Vercel app at cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173", "https://meal-craft-frontend.vercel.app"],
     credentials: true
 }))
 
