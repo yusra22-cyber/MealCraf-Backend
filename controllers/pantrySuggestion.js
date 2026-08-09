e59cef90f15b9dd808de76956fdbf7845c0553e8
Yusra <laraibyusra519@gmail.com>
2026-08-09 09:20:56 -0700

    MealCraft Backend

--- controllers/pantrySuggestion.js
+++ controllers/pantrySuggestion.js
@@ -1,22 +1,22 @@
 const systemRecepie = require("../models/systemRecepie")
 const pantryItem = require("../models/pantryItem")
+const pluralize = require("pluralize")
 
 async function handlePantrySuggestion(req,res){
     try {
         
         const user = await pantryItem.find({user:req.user.id})
-        const ingedients= user.map(item=>item.ingredientName.toLowerCase())
+        const ingedients = user.map(item => pluralize.singular(item.ingredientName.toLowerCase()))
         const systemIngredients = await systemRecepie.find({}).select("title category ingredients")
 
-        
         const systemItems = systemIngredients.map(item=>{
 
             ////  matched item
          const  matched =  item.ingredients.filter(i=>
-                (ingedients.includes(i.ingredientName.toLowerCase()))).map(i=>i.ingredientName)
+                (ingedients.includes(pluralize.singular(i.ingredientName.toLowerCase())))).map(i=>i.ingredientName)
             ////  missing items
          const missedIngredients = item.ingredients.filter(i=>
-            (!ingedients.includes(i.ingredientName.toLowerCase()))).map(i=>i.ingredientName)
+            (!ingedients.includes(pluralize.singular(i.ingredientName.toLowerCase())))).map(i=>i.ingredientName)
                 return{
                     _id: item._id,
                     title:item.title,
@@ -26,9 +26,6 @@ async function handlePantrySuggestion(req,res){
                     totalMatched: matched.length,
                     totalIngrdients:item.ingredients.length,
                 }}).filter(recepie=>recepie.totalMatched>0)
-        
-
-        
         
         return res.json({msg:"Sugested recepies",systemItems,totalPantry:ingedients.length})
 
